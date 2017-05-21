7e0f5cf846c104715a2d1a7842c3736c05d29c67
Walle Cyril <capocyril@hotmail.com>
2017-05-21 15:36:08 +0200

    c

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-# Fetch Work
+# Work
+
 
 ## Why
 
@@ -19,3 +20,37 @@ Import this library and read this readme.
 results for ping
 average rountrip time for korean.net 320ms
 average rountrip time for google.com 30ms
+
+
+## Install
+
+Download `work.js`
+
+
+## API
+
+the function must be synchronous, to garantee in order receival, but also because of how, the return value is used
+
+
+## Hope
+
+ * 6+ no effect
+ * 5 immediately terminated after use and all the above
+ * 0 immediately unregistered/deleted after use and all the above
+
+
+## Lazy
+
+ * 5+ no effect
+ * 4 and all the above
+ * 3 and all the above
+ * 2 and all the above
+ * 1 and all the above
+ * 0 and all the above
+
+
+##Alternatives
+
+https://github.com/andywer/threads.js
+https://github.com/padolsey/operative
+raw web worker
--- source/work.js
+++ source/work.js
@@ -1,11 +1,9 @@
-"use strict";
-/* the function must be synchronous, to garantee in order receival, but also because of how, the return value is used
+/*work.js*/
+/*
 
 other options: timeOut (after how many ms, should we cancel the operation)
 
 auto split (spawn more worker instances)2-4-8-16 auto based on feedback
-
-implement lazy and hope
 
 expose decorate to compile time
 
@@ -13,9 +11,7 @@ navigator.hardwareConcurrency
 
 const URL = window.URL || window.webkitURL;
 
-Recursion
-
-Alternatives https://github.com/andywer/threads.js https://github.com/padolsey/operative
+allow Recursion inside worker
 
 report progress (use Date.now as id, but allow dependency injection when needed)
 
@@ -28,8 +24,19 @@ multiple results callback
 
 convention pass done function so that setTimeOut works
 
-what really happens after terminate
+what really happens after terminate ?
+
+optimisation ideas
+
+remove unused things inside worker, like originalAsString if it is never going to be used again
 */
+/*jslint
+    es6, maxerr: 50, browser, devel, fudge, maxlen: 100
+*/
+/*global
+    Worker, URL, Blob
+*/
+"use strict";
 const workers = {};
 
 const SYMBOLS = {
@@ -46,7 +53,7 @@ const WORKER_DEFAULT_OPTIONS = {
     ressource: "/js/myWorker.js",
     loadMode: SYMBOLS.STRING,
     lazy: 10,
-    hope: 1,
+    hope: 1
 };
 
 // separate from WORKER_DEFAULT_OPTIONS, impossible to overwrite with options in registerWorker
@@ -56,7 +63,8 @@ const WORKER_INITIAL_SETTINGS = {
     decorated: false,
     decoratedAsString: "",
     instanciated: false,
-    instance: {},
+    workerObjectURL: undefined,
+    instance: undefined,
     awakened: false,
     hasEventListener: false,
     resolutionQueue: []
@@ -68,7 +76,7 @@ const loadWorker = function (worker) {
     if (loadMode === SYMBOLS.FUNCTION) {
         worker.originalAsString = ressource.toString();
     }
-    worker.laoded = true;
+    worker.loaded = true;
 };
 
 const decorateWorker = function (worker) {
@@ -79,6 +87,9 @@ const decorateWorker = function (worker) {
 const doWork = ${originalAsString};
 self.addEventListener("message", function(event) {
     const message = event.data;
+    if (!Object.prototype.hasOwnProperty.call(message, "input")) {
+        return; // only waking up
+    }
     const input = message.input;
     self.postMessage({
         result: doWork(...input)
@@ -91,10 +102,22 @@ self.addEventListener("message", function(event) {
 
 const instanciateWorker = function (worker) {
     const decoratedAsString = worker.decoratedAsString;
-    const workerBlob = new Blob([decoratedAsString], { type: "text/javascript" });
-    const workerObjectURL = URL.createObjectURL(workerBlob);
+    let workerObjectURL;
+    if (worker.workerObjectURL) {
+        workerObjectURL = worker.workerObjectURL;
+    } else {
+        const workerBlob = new Blob([decoratedAsString], { type: "text/javascript" });
+        workerObjectURL = URL.createObjectURL(workerBlob);
+    }
     const instance = new Worker(workerObjectURL);
-    URL.revokeObjectURL(workerObjectURL);
+    if (worker.hope > 5) {
+        // with so much hope the worker will never be terminated
+        // remove for debugging
+        URL.revokeObjectURL(workerObjectURL);
+    } else {
+        // keep for reuse
+        worker.workerObjectURL = workerObjectURL;
+    }
     worker.instance = instance;
     worker.instanciated = true;
 };
@@ -108,52 +131,78 @@ const forceAwakenWorker = function (worker) {
     worker.awakened = true; // or will be in a few
 };
 
+const evaluateWorker = function (worker) {
+    /* a worker with 0 hope was made to be used 1 time
+    a worker with 100 hope was made to be used multiple times*/
+    const hope = worker.hope;
+    if (hope > 5) {
+        return;
+    }
+    worker.instance.terminate();
+    worker.instance = undefined;
+    worker.instanciated = false;
+    worker.awakened = false;
+    worker.hasEventListener = false;
+
+    if (hope > 0) {
+        return;
+    }
+    delete workers[worker.name];
+};
+
 const addEventListenerToWorker = function (worker) {
     const instance = worker.instance;
     instance.addEventListener("message", function(event) {
         const message = event.data;
         const result = message.result;
-        if (worker.resolutionQueue.length !== 0) {
+        let length = worker.resolutionQueue.length;
+        if (length !== 0) {
             const nextResolve = worker.resolutionQueue.shift();
             nextResolve(result);
+            length = worker.resolutionQueue.length;
         } /* else report an error ? */
+
+        if (length === 0) {
+            evaluateWorker(worker);
+        }
     });
     worker.hasEventListener = true;
 };
 
 const prepareWorker = function (worker, lazy, makesSenseToAwake = false) {
     // lazy is used for cascade pattern
-    if (lazy > 5) {
+    if (lazy > 4) {
         return;
     }
     if (!worker.loaded) {
         loadWorker(worker);
     }
-    if (lazy > 4) {
+    if (lazy > 3) {
         return;
     }
     if (!worker.decorated) {
         decorateWorker(worker);
     }
-    if (lazy > 3) {
+    if (lazy > 2) {
         return;
     }
     if (!worker.instanciated) {
         instanciateWorker(worker);
     }
-    if (lazy > 2) {
+    if (lazy > 1) {
         return;
     }
     if (!worker.hasEventListener) {
         addEventListenerToWorker(worker);
     }
-    if (lazy > 1) {
+    if (lazy > 0) {
         return;
     }
     if (!worker.awakened && makesSenseToAwake) {
         forceAwakenWorker(worker);
     }
 };
+
 const registerWorker = function (options) {
     const worker = Object.assign(
         {},
@@ -162,21 +211,21 @@ const registerWorker = function (options) {
         WORKER_INITIAL_SETTINGS
     );
 
-    workers[worker.name] = worker;
     const loadMode = worker.loadMode;
     const ressource = worker.ressource;
     if (loadMode === SYMBOLS.STRING) {
         worker.originalAsString = ressource;
-        worker.laoded = true;
-    } else if (loadMode === SYMBOLS.STRING) {
+        worker.loaded = true;
+    } else if (loadMode === SYMBOLS.DECORATED) {
         worker.decoratedAsString = ressource;
         worker.decorated = true;
-        worker.laoded = true;
-    } 
+        worker.loaded = true;
+    }
     prepareWorker(worker, worker.lazy, true);
+    workers[worker.name] = worker;
 };
 
-const fetchWork = function (name, input = []) {
+const work = function (name, input = []) {
     return new Promise(function (resolve, reject) {
         if (!Object.prototype.hasOwnProperty.call(workers, name)) {
             reject(`"${name}" not registered`);
@@ -187,6 +236,7 @@ const fetchWork = function (name, input = []) {
         worker.instance.postMessage({
             input
         });
+        worker.awakened = true;
     });
 };
 
@@ -197,11 +247,20 @@ registerWorker({
                     return a + b;
                 }`,
     loadMode: "string",
-    lazy: 10,
-    hope: 1,
+    lazy: 0,
+    hope: 1
 });
 
-fetchWork("doSomething", [5, 4]).then(function (x) {
-    console.log(x);
+work("doSomething", [5, 4]).then(function (x) {
+    console.log("#1", x);
+    work("doSomething", [x, 4]).then(function (y) {
+        console.log("#2", y);
+    });
 });
-//
+// work("doSomething", [5, 4]).then(function (x) {
+    // console.log("#1", x);
+
+// });
+// work("doSomething", [40, 40]).then(function (y) {
+    // console.log("#2", y);
+// });
