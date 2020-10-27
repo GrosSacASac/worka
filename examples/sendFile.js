db97a769ace092550b4c1b293c55e3d73a422390
Walle Cyril <cyril.walle@protonmail.com>
2020-10-27 01:29:03 +0100

    Update sendFile.js

--- examples/sendFile.js
+++ examples/sendFile.js
@@ -1,7 +1,8 @@
 export { makeSendFileAvailable };
 import { createReadStream } from "fs";
 
-
+// probably not the right way to do it
+// todo
 const makeSendFileAvailable = function (req, res, next) {
     res.sendFile = sendFile;
     next();
